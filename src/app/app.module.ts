dee2b1b47b5cccbeab837e466260f7e4576287ef
Arunkumar Angappan <arunak283933@gmail.com>
2019-06-13 19:51:10 +0530

    13/06/19 initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { TeamComponent } from './team/team.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { ContactComponent } from './contact/contact.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { AboutComponent } from './about/about.component';
+import { ClientComponent } from './client/client.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    TeamComponent,
+    ProjectsComponent,
+    TechnologyComponent,
+    ContactComponent,
+    TopbarComponent,
+    AboutComponent,
+    ClientComponent,
+    TestimonialsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
